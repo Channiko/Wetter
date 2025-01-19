9be1a3e6e821353dfac34f12094f7c1d7b3fda10
Channiko <91826707+Channiko@users.noreply.github.com>
2025-01-19 23:40:05 +0100

    added search dunctionality using api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+function refreshweater(response) {
+  let temperatureElement = document.querySelector("#temperature")
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+   let cityElement = document.querySelector("#city")
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apikey = "201obc05f8b3t414d38b1ae0d468cf87";
+  let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`
+  axios.get(apiurl).then(refreshweater);
+}
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city")
-  cityElement.innerHTML = searchInput.value;
-
+ 
+searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Antwerp");
